1123f0ad2fd92538f2749b28f68646606003dbfe
Snehashis Roy <116051236+snehashisroyofficial@users.noreply.github.com>
2024-06-06 09:15:57 +0530

    pay button modal added

---
+++ src/Components/Modal/Modal.jsx
@@ -0,0 +1,120 @@
+import { useRef, useState } from "react";
+import DatePicker from "react-datepicker";
+import { IoMdClose } from "react-icons/io";
+const Modal = ({ data, onClose }) => {
+  const [year, setYear] = useState(null);
+
+  const modalRef = useRef();
+
+  const closeModal = (e) => {
+    if (modalRef.current === e.target) {
+      onClose();
+    }
+  };
+  return (
+    <div
+      ref={modalRef}
+      onClick={closeModal}
+      className="fixed inset-0 bg-black bg-opacity-70 flex justify-center items-center "
+    >
+      <div className="max-w-md flex flex-col items-center justify-center   gap-5 ">
+        <button onClick={onClose} className="place-self-end ">
+          <IoMdClose className="text-3xl text-white" />
+        </button>
+
+        <div className="bg-white p-10 rounded-lg">
+          <h1 className="text-2xl text-center font-extrabold"> Pay Salary</h1>
+          <form>
+            {/* employee name  */}
+            <div className="mt-6">
+              <label
+                className="text-gray-700 dark:text-gray-200"
+                htmlFor="password"
+              >
+                Employee Name
+              </label>
+              <input
+                defaultValue={data.name}
+                disabled
+                name="name"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+              />
+            </div>
+            {/* salary  */}
+            <div className="mt-6">
+              <label
+                className="text-gray-700 dark:text-gray-200"
+                htmlFor="password"
+              >
+                Salary
+              </label>
+              <input
+                type="number"
+                name="salary"
+                defaultValue={data.salary}
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+              />
+            </div>
+
+            <div className="flex gap-2 items-center">
+              {/* date  */}
+              <div className="mt-4 w-full ">
+                <label
+                  className="text-gray-700 dark:text-gray-200"
+                  htmlFor="password"
+                >
+                  Month
+                </label>
+                <select
+                  defaultValue=""
+                  name="month"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                >
+                  <option disabled value="">
+                    Choose month
+                  </option>
+                  <option value="january">January</option>
+                  <option value="february">February</option>
+                  <option value="march">March</option>
+                  <option value="april">April</option>
+                  <option value="may">May</option>
+                  <option value="june">June</option>
+                  <option value="july">July</option>
+                  <option value="august">August</option>
+                  <option value="september">September</option>
+                  <option value="october">October</option>
+                  <option value="november">November</option>
+                  <option value="december">December</option>
+                </select>
+              </div>
+
+              {/* year  */}
+              <div className="mt-4">
+                <label htmlFor="year">Year</label>
+                <div className="w-full">
+                  <DatePicker
+                    className=" w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                    selected={year}
+                    onChange={(date) => setYear(date)}
+                    showYearPicker
+                    dateFormat="yyyy"
+                    placeholderText="Select a year"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="mt-6">
+              <button className="btn bg-green-600 text-white w-full ">
+                {" "}
+                Pay
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
--- src/Pages/Dashboard/Hr/EmployeeList.jsx
+++ src/Pages/Dashboard/Hr/EmployeeList.jsx
@@ -3,11 +3,12 @@ import useAxiosPublic from "../../../Hooks/useAxiosPublic";
 import { FaCheckCircle } from "react-icons/fa";
 import { MdCancel } from "react-icons/md";
 import { useQuery } from "@tanstack/react-query";
-import DatePicker from "react-datepicker";
 import Swal from "sweetalert2";
+import Modal from "../../../Components/Modal/Modal";
 const EmployeeList = () => {
   const axiosPublic = useAxiosPublic();
-  const [selectedYear, setSelectedYear] = useState(null);
+  const [data, setData] = useState([]);
+  const [showModal, setShowModal] = useState(false);
 
   const { data: workdata, refetch } = useQuery({
     queryKey: ["workdata"],
@@ -38,17 +39,10 @@ const EmployeeList = () => {
       });
   };
 
-  const handlePay = (email) => {
-    console.log(email);
-    document.getElementById("my_modal_3").showModal();
-    axiosPublic
-      .get(`/work-sheet/${email}`)
-      .then((res) => {
-        console.log(res.data);
-      })
-      .catch((error) => {
-        console.log(error.message);
-      });
+  const handleDoubleClick = (work) => {
+    setData("");
+    setData(work);
+    setShowModal(true);
   };
 
   return (
@@ -87,7 +81,7 @@ const EmployeeList = () => {
                 <td>
                   {work.isVerified === "true" ? (
                     <button
-                      onClick={() => handlePay(work.email)}
+                      onClick={() => handleDoubleClick(work)}
                       className="btn  bg-success text-white"
                     >
                       {" "}
@@ -103,93 +97,15 @@ const EmployeeList = () => {
                 <td>
                   <button className="btn ">View Details</button>
                 </td>
-                <dialog id="my_modal_3" className="modal">
-                  <div className="modal-box ">
-                    <form method="dialog">
-                      {/* if there is a button in form, it will close the modal */}
-                      <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
-                        ✕
-                      </button>
-                    </form>
-                    <h3 className="font-bold text-2xl text-center">
-                      Pay Salary{" "}
-                    </h3>
-
-                    {/* employee name  */}
-                    <div className="mt-6">
-                      <label
-                        className="text-gray-700 dark:text-gray-200"
-                        htmlFor="password"
-                      >
-                        Employee Name
-                      </label>
-                      <input
-                        defaultValue={work.name}
-                        disabled
-                        className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
-                      />
-                    </div>
-
-                    <div className="flex gap-2 items-center">
-                      {/* date  */}
-                      <div className="mt-4 w-full ">
-                        <label
-                          className="text-gray-700 dark:text-gray-200"
-                          htmlFor="password"
-                        >
-                          Date
-                        </label>
-                        <select
-                          defaultValue=""
-                          name="task"
-                          className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
-                        >
-                          <option disabled value="">
-                            Choose month
-                          </option>
-                          <option value="january">January</option>
-                          <option value="february">February</option>
-                          <option value="march">March</option>
-                          <option value="april">April</option>
-                          <option value="may">May</option>
-                          <option value="june">June</option>
-                          <option value="july">July</option>
-                          <option value="august">August</option>
-                          <option value="september">September</option>
-                          <option value="october">October</option>
-                          <option value="november">November</option>
-                          <option value="december">December</option>
-                        </select>
-                      </div>
-
-                      {/* year  */}
-                      <div className="mt-4">
-                        <label htmlFor="year">Year</label>
-                        <div className="w-full">
-                          <DatePicker
-                            className=" w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
-                            selected={selectedYear}
-                            onChange={(date) => setSelectedYear(date)}
-                            showYearPicker
-                            dateFormat="yyyy"
-                            placeholderText="Select a year"
-                          />
-                        </div>
-                      </div>
-                    </div>
-
-                    <div className="mt-6">
-                      <button className="btn bg-green-600 text-white w-full ">
-                        {" "}
-                        Pay
-                      </button>
-                    </div>
-                  </div>
-                </dialog>
               </tr>
             ))}
           </tbody>
         </table>
+        <div className="transition ease-in-out delay-150">
+          {showModal && (
+            <Modal data={data} onClose={() => setShowModal(false)} />
+          )}
+        </div>
       </div>
     </div>
   );
