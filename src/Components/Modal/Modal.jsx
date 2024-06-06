4a9a417faae5868a6e5fdf58d19838bf0ae08d3b
Snehashis Roy <116051236+snehashisroyofficial@users.noreply.github.com>
2024-06-06 09:22:13 +0530

    modal successfully working

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -2,7 +2,7 @@ import { useRef, useState } from "react";
 import DatePicker from "react-datepicker";
 import { IoMdClose } from "react-icons/io";
 const Modal = ({ data, onClose }) => {
-  const [year, setYear] = useState(null);
+  const [showyear, setYear] = useState(null);
 
   const modalRef = useRef();
 
@@ -11,6 +11,20 @@ const Modal = ({ data, onClose }) => {
       onClose();
     }
   };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = data.email;
+    const salary = form.salary.value;
+    const month = form.month.value;
+    const year = showyear;
+
+    const formData = { name, email, salary, month, year };
+    console.log(formData);
+  };
+
   return (
     <div
       ref={modalRef}
@@ -24,7 +38,7 @@ const Modal = ({ data, onClose }) => {
 
         <div className="bg-white p-10 rounded-lg">
           <h1 className="text-2xl text-center font-extrabold"> Pay Salary</h1>
-          <form>
+          <form onSubmit={handleFormSubmit}>
             {/* employee name  */}
             <div className="mt-6">
               <label
@@ -51,6 +65,7 @@ const Modal = ({ data, onClose }) => {
               <input
                 type="number"
                 name="salary"
+                required
                 defaultValue={data.salary}
                 className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
               />
@@ -68,6 +83,7 @@ const Modal = ({ data, onClose }) => {
                 <select
                   defaultValue=""
                   name="month"
+                  required
                   className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
                 >
                   <option disabled value="">
@@ -93,8 +109,9 @@ const Modal = ({ data, onClose }) => {
                 <label htmlFor="year">Year</label>
                 <div className="w-full">
                   <DatePicker
+                    required
                     className=" w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
-                    selected={year}
+                    selected={showyear}
                     onChange={(date) => setYear(date)}
                     showYearPicker
                     dateFormat="yyyy"
