b0cc2db6e5975f122f01829636423da1b7101dd0
Snehashis Roy <116051236+snehashisroyofficial@users.noreply.github.com>
2024-06-08 08:21:04 +0530

    hr dashboard fixed

--- src/Pages/Dashboard/Hr/Progress/Progress.jsx
+++ src/Pages/Dashboard/Hr/Progress/Progress.jsx
@@ -56,58 +56,7 @@ const Progress = () => {
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <div className="flex justify-center items-center gap-12">
-          <div>
-            <select
-              defaultValue=""
-              name="name"
-              className="block px-4 py-2  text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
-            >
-              <option disabled value="">
-                Choose Employee Name
-              </option>
-
-              {[...new Set(work.map((works) => works.name))].map((unq, idx) => (
-                <option key={idx} value={unq}>
-                  {unq}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div>
-            <select
-              defaultValue=""
-              name="month"
-              className="block px-4 py-2  text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
-            >
-              <option disabled value="">
-                Choose month
-              </option>
-              <option value="January">January</option>
-              <option value="February">February</option>
-              <option value="March">March</option>
-              <option value="April">April</option>
-              <option value="May">May</option>
-              <option value="June">June</option>
-              <option value="July">July</option>
-              <option value="August">August</option>
-              <option value="September">September</option>
-              <option value="October">October</option>
-              <option value="November">November</option>
-              <option value="December">December</option>
-            </select>
-          </div>
-          <div>
-            <button type="submit" className="btn bg-blue-700 text-white ">
-              <FaSearch />
-              Search
-            </button>
-          </div>
-        </div>
-      </form>
-
-      <div className="flex gap-6 my-6">
+      <div className="flex justify-center gap-6 my-6">
         <div>
           {totalWorkingHours && (
             <div className="p-4 rounded-lg space-y-4 bg-blue-100 w-fit">
@@ -134,12 +83,69 @@ const Progress = () => {
             </div>
           )}
         </div>
+
+        <form
+          className="p-4 rounded-lg space-y-4 bg-green-100 w-fit"
+          onSubmit={handleSubmit}
+        >
+          <h2>Filter Data</h2>
+          <div className="flex justify-center items-center gap-12">
+            <div>
+              <select
+                defaultValue=""
+                name="name"
+                className="block px-4 py-2  text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+              >
+                <option disabled value="">
+                  Choose Employee Name
+                </option>
+
+                {[...new Set(work.map((works) => works.name))].map(
+                  (unq, idx) => (
+                    <option key={idx} value={unq}>
+                      {unq}
+                    </option>
+                  )
+                )}
+              </select>
+            </div>
+            <div>
+              <select
+                defaultValue=""
+                name="month"
+                className="block px-4 py-2  text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+              >
+                <option disabled value="">
+                  Choose month
+                </option>
+                <option value="January">January</option>
+                <option value="February">February</option>
+                <option value="March">March</option>
+                <option value="April">April</option>
+                <option value="May">May</option>
+                <option value="June">June</option>
+                <option value="July">July</option>
+                <option value="August">August</option>
+                <option value="September">September</option>
+                <option value="October">October</option>
+                <option value="November">November</option>
+                <option value="December">December</option>
+              </select>
+            </div>
+            <div>
+              <button type="submit" className="btn bg-blue-700 text-white ">
+                <FaSearch />
+                Search
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
       <div className="max-w-6xl  mx-auto p-2  sm:p-4 ">
         <h2 className="mb-4 text-2xl font-semibold leading-tight">All Tasks</h2>
         <div className="overflow-x-auto">
           <table className="min-w-full  ">
-            <thead className="bg-orange-300">
+            <thead className="bg-gray-300/30">
               <tr className="text-left ">
                 <th className="p-3 text-center">Employee Name</th>
                 <th className="p-3 text-center">Task</th>
@@ -152,7 +158,7 @@ const Progress = () => {
                 ? filterData.map((data, idx) => (
                     <tr
                       key={idx}
-                      className="border-b border-opacity-20 border-orange-300 bg-orange-50"
+                      className="border-b border-opacity-20 border-gray-500 "
                     >
                       <td className="p-3 text-center">
                         <p>{data.name}</p>
@@ -171,7 +177,7 @@ const Progress = () => {
                 : work.map((data, idx) => (
                     <tr
                       key={idx}
-                      className="border-b border-opacity-20 border-orange-300 bg-orange-50"
+                      className="border-b border-opacity-20 border-gray-500 "
                     >
                       <td className="p-3 text-center">
                         <p>{data.name}</p>
