7501eb50f584716e1154d6164189b14b9548d2b8
Snehashis Roy <116051236+snehashisroyofficial@users.noreply.github.com>
2024-06-10 00:32:09 +0530

    night work ]

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -8,28 +8,35 @@ import { FaUsers } from "react-icons/fa";
 import { RiProgress3Line } from "react-icons/ri";
 import { MdOutlineContactSupport } from "react-icons/md";
 import useRole from "../Hooks/useRole";
+import moment from "moment";
+
 //
 
 const Dashboard = () => {
   const { user } = useAuth();
+  console.log(user);
   const [role, isLoading] = useRole();
   const employeeLinks = (
     <>
-      <li>
+      <li className="  flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700">
         <NavLink
           to="/dashboard/work-sheet"
           className={({ isActive }) =>
-            isActive ? "text-orange-600 w-fit" : "hover:bg-yellow-200 "
+            isActive
+              ? "text-blue-600  flex items-center  gap-4"
+              : " flex items-center  gap-4"
           }
         >
           <MdWorkOutline /> Work Sheet
         </NavLink>
       </li>
-      <li>
+      <li className="  flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700">
         <NavLink
           to="/dashboard/payment-history"
           className={({ isActive }) =>
-            isActive ? "text-orange-600 w-fit" : "hover:bg-yellow-200 "
+            isActive
+              ? "text-blue-600  flex items-center  gap-4"
+              : " flex items-center  gap-4"
           }
         >
           <MdPayment /> Payment History
@@ -39,21 +46,25 @@ const Dashboard = () => {
   );
   const hrLinks = (
     <>
-      <li>
+      <li className="  flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700">
         <NavLink
           to="/dashboard/employee-list"
           className={({ isActive }) =>
-            isActive ? "text-orange-600 w-fit" : "hover:bg-yellow-200 "
+            isActive
+              ? "text-blue-600  flex items-center  gap-4"
+              : " flex items-center  gap-4"
           }
         >
           <FaUsers /> Employee List
         </NavLink>
       </li>
-      <li>
+      <li className="  flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700">
         <NavLink
           to="/dashboard/progress"
           className={({ isActive }) =>
-            isActive ? "text-orange-600 w-fit" : "hover:bg-yellow-200 "
+            isActive
+              ? "text-blue-600  flex items-center  gap-4"
+              : " flex items-center  gap-4"
           }
         >
           <RiProgress3Line /> Progress
@@ -63,21 +74,25 @@ const Dashboard = () => {
   );
   const adminLinks = (
     <>
-      <li>
+      <li className="  flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700">
         <NavLink
           to="/dashboard/all-employee-list"
           className={({ isActive }) =>
-            isActive ? "text-orange-600 w-fit" : "hover:bg-yellow-200 "
+            isActive
+              ? "text-blue-600  rounded-lg  flex items-center  gap-4"
+              : " flex items-center  gap-4"
           }
         >
           <FaUsers /> All Employee List
         </NavLink>
       </li>
-      <li>
+      <li className="  flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700">
         <NavLink
           to="/dashboard/contact-us"
           className={({ isActive }) =>
-            isActive ? "text-orange-600 w-fit" : "hover:bg-yellow-200 "
+            isActive
+              ? "text-blue-600  flex items-center  gap-4"
+              : " flex items-center  gap-4"
           }
         >
           <MdOutlineContactSupport /> Contact us
@@ -95,36 +110,73 @@ const Dashboard = () => {
   }
 
   return (
-    <div className=" rounded-xl flex font-body ">
-      <div className="min-h-screen w-[20% ]  bg-gray-200 menu  p-10">
-        {user && (
-          <h1 className="text-4xl font-bold">Hi, {user?.displayName}</h1>
-        )}
+    <div className=" rounded-xl flex font-body bg-gray-300 ">
+      <div className="min-h-screen w-1/6 px-4 py-8  ">
+        <div className="min-h-screen  bg-white px-8 rounded-3xl ">
+          <h1
+            className={`text-2xl text-center  uppercase p-2    ${
+              role === "admin"
+                ? "text-red-700 bg-red-100 rounded-b-xl"
+                : role === "hr"
+                ? "text-orange-500 bg-orange-100 rounded-b-xl "
+                : role === "employee"
+                ? "text-blue-700 bg-blue-100 rounded-b-xl"
+                : null
+            }  font-bold `}
+          >
+            {role} Dashboard
+          </h1>
+          {user && (
+            <div class="flex flex-col items-center mt-6 -mx-2">
+              <img
+                class="object-cover w-24 h-24 mx-2 rounded-full"
+                src={user?.photoURL}
+                alt="avatar"
+              />
+              <h4 class="mx-2 mt-2 font-medium text-gray-800 dark:text-gray-200">
+                {user?.displayName}
+              </h4>
+              <p class="mx-2 mt-1 text-sm font-medium text-gray-600 dark:text-gray-400">
+                {user?.email}
+              </p>
+              <p class="mx-2 mt-1 text-xs font-medium text-gray-600 dark:text-gray-400">
+                Last Login:{" "}
+                {moment(user?.metadata?.lastSignInTime).format(
+                  "MMMM Do YYYY, h:mm:ss a"
+                )}
+              </p>
+            </div>
+          )}
 
-        <h1 className="text-2xl mt-10 capitalize">{role} Dashboard</h1>
-        <ul className="font-semibold text-xl space-y-4 py-10">
-          {role === "admin"
-            ? adminLinks
-            : role === "hr"
-            ? hrLinks
-            : role === "employee"
-            ? employeeLinks
-            : null}
-          <li>
-            <NavLink
-              to="/"
-              className={({ isActive }) =>
-                isActive ? "text-orange-600 w-fit" : "hover:bg-yellow-200 "
-              }
-            >
-              <GrHomeRounded />
-              Home
-            </NavLink>
-          </li>
-        </ul>
+          <ul className="font-semibold text-xl space-y-4 py-10">
+            {role === "admin"
+              ? adminLinks
+              : role === "hr"
+              ? hrLinks
+              : role === "employee"
+              ? employeeLinks
+              : null}
+            <li className="  flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700">
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-orange-600  flex items-center  gap-4"
+                    : " flex items-center  gap-4"
+                }
+              >
+                <GrHomeRounded />
+                Home
+              </NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="flex-1 p-10">
-        <Outlet />
+
+      <div className="flex-1  p-8 ">
+        <div className="flex-1 p-10 bg-white h-screen rounded-3xl ">
+          <Outlet />
+        </div>
       </div>
     </div>
   );
